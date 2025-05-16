14055fa71fbb668e24168bf00f7cdf66da3b4aee
faheemnaveed07 <faheemnaveed09@gmail.com>
2025-05-16 15:51:58 +0500

    utils folder created

---
+++ lib/login_page.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          Text(
+            'This is Google Fonts',
+            style: GoogleFonts.lato(
+              textStyle: TextStyle(color: Colors.blue, letterSpacing: .5),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/utils/constants.dart
@@ -0,0 +1,3 @@
+import 'package:flutter/material.dart';
+
+var myPrimaryColor=Colors.blue;
